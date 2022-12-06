99993f7d155fc19fae59594bf157e0217e6bde33
tysondanby <tyson.danby@hotmail.com>
2022-12-05 20:25:28 -0700

    updated maneuvers

--- run.jl
+++ run.jl
@@ -3,15 +3,17 @@ include("main.jl")
 excelfile = "Simulation_Setup.xlsx"
 
 #-------Start Time
+days = 0.0
 hours= 0.0
 minutes = 0.0
 seconds = 0.0
-tstart = 3600*hours+60*minutes+seconds
+tstart = 3600*(24*days+hours)+60*minutes+seconds
 #-------Finish Time
-hours= 1.0
+days = 10.0
+hours= 0.0
 minutes = 0.0
 seconds = 0.0
-tfinish = 3600*hours+60*minutes+seconds
+tfinish = 3600*(24*days+hours)+60*minutes+seconds
 
 framesplotted = ["Earth","Moon"]
 pointsplotted = 1000
